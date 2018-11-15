988feca16d74dcba968e4f4bdc7dbc7cbac6675d
Luc <luc.lemahieu@laposte.net>
2018-11-15 14:42:27 +0100

    preparation des exo

---
+++ exercice1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <link href="style.css" />
+    </head>
+    <body>
+        <?php
+            $age = 18;
+                if($age>= 18){
+                    echo 'Tu es majeur';
+                }else{
+                    echo 'Tu es mineur';
+                }
+            
+        ?>
+    </body>
+</html>
---
+++ exercice7/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <link href="style.css" />
+        <title>Exercice 7 PHP partie 2</title>
+    </head>
+    <body>
+        <?php
+
+        ?>
+    </body>
+</html>
